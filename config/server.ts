e0668f910ec705adfbcaa5e56269f947aa957dc6
mohamed-magdy-1 <King44123no@gamil.com>
2025-04-01 20:19:50 +0200

    updated

--- config/server.ts
+++ config/server.ts
@@ -1,16 +1,16 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('BACKEND_URL', 'http://localhost:1337'), // عنوان URL لـ Strapi
+  url: env('BACKEND_URL', 'https://shop-co-back-end.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // إضافة هذا الجزء لتحديد عنوان URL الأمامي
+  // إعدادات لوحة الإدارة
   admin: {
-    url: env('ADMIN_URL', '/admin'), // للوحة الإدارة
+    url: env('ADMIN_URL', '/admin'),
     forgotPassword: {
       emailTemplate: {
-        link: `${env('FRONTEND_URL', 'http://localhost:3000')}/reset-password?code={token}`,
+        link: `${env('FRONTEND_URL', 'https://your-frontend-url.com')}/reset-password?code={token}`,
       },
     },
   },
