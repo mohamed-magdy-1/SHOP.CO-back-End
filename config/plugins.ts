7136c72347cb64a68c4d9f4f75079e44f156aa8c
mohamed-magdy-1 <King44123no@gamil.com>
2025-04-02 09:17:31 +0200

    new updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -18,4 +18,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
